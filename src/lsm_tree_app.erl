22e2b1a9d4867a653fe223c7bc4c1012dcccc2a7
Gregory Burd <greg@basho.com>
2012-06-29 20:00:37 +0530

    Add copyright headers

--- src/lsm_tree_app.erl
+++ src/lsm_tree_app.erl
@@ -1,3 +1,27 @@
+%% -*- coding: utf-8; Mode: erlang; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+%% ex: set softtabstop=4 tabstop=4 shiftwidth=4 expandtab fileencoding=utf-8:
+
+%% ----------------------------------------------------------------------------
+%%
+%% lsm_tree: A Riak/KV backend using SQLite4's Log-Structured Merge Tree
+%%
+%% Copyright 2012 (c) Basho Technologies, Inc.  All Rights Reserved.
+%% http://basho.com/ info@basho.com
+%%
+%% This file is provided to you under the Apache License, Version 2.0 (the
+%% "License"); you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+%% License for the specific language governing permissions and limitations
+%% under the License.
+%%
+%% ----------------------------------------------------------------------------
+
 -module(lsm_tree_app).
 
 -behaviour(application).
